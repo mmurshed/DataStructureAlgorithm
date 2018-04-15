54fbd2f482e9db3d97de1e54231a31dab91cfc52
Mahbub Murshed <mahbub@Mahbubs-MacBook-Pro.local>
2018-04-15 16:55:15 -0500

    Added stack based depth first search

---
+++ DataStructurePractice/Graph/Enumerator/DepthFirstEnumeratorStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    public class DepthFirstEnumeratorStack<V, E> : GraphEnumerator<V, E>
+    {
+        public DepthFirstEnumeratorStack(IGraph2<V, E> graph) : base(graph) { }
+
+        protected override IEnumerable<IVertex2<V>> Search(IVertex2<V> root)
+        {
+            if (IsVisited(root))
+                yield break;
+           
+            var stack = new Stack<IVertex2<V>>();
+
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+
+                if (!IsVisited(vertex))
+                {
+                    SetVisited(vertex);
+                    yield return vertex;
+
+                    foreach (var neighbour in graph.GetNeighbours(vertex))
+                    {
+                        stack.Push(neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DataStructurePractice/Graph/Visitor/DepthFirstSearchStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    // Alternate implemented of depth first using stack. It's exactly the same
+    // As the BreadthFirstSearch but using Stack instead of queue.
+    public class DepthFirstSearchStack<V, E> : GraphSearch<V, E>
+    {
+        public override void Search(IVertex<V> root, IVisitor<IVertex<V>> visitor)
+        {
+            if (IsVisited(root))
+                return;
+
+            Stack<IVertex<V>> stack = new Stack<IVertex<V>>();
+
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+
+                if (!IsVisited(vertex))
+                {
+                    visitor.PreVisit(vertex);
+                    SetVisited(vertex);
+
+                    foreach (var neighbour in vertex.Neighbours)
+                    {
+                        stack.Push(neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
