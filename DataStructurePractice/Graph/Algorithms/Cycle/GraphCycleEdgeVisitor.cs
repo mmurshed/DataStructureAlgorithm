ccc939a6b40deb7260b4a89548185a38ed0b063d
Mahbub Murshed <mahbub@Mahbubs-MacBook-Pro.local>
2018-04-15 23:39:43 -0500

    Cycle detection

---
+++ DataStructurePractice/Graph/Algorithms/Cycle/GraphCycleEdgeVisitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    public class GraphCycleEdgeVisitor<V, E> : IVisitor<IEdge<V, E>>
+    {
+        private Dictionary<IVertex<V>, IVertex<V>> parent;
+        private readonly GraphSearch<V, E> Search;
+
+        public bool HasCycle { get; private set; }
+
+        public GraphCycleEdgeVisitor(GraphSearch<V, E> search)
+        {
+            parent = new Dictionary<IVertex<V>, IVertex<V>>();
+            Search = search;
+
+            HasCycle = false;
+        }
+
+        public void PreVisit(IEdge<V, E> value)
+        {
+        }
+
+        public void Visit(IEdge<V, E> value)
+        {
+            if(Search.IsVisited(value.Start) && parent.ContainsKey(value.Start) && parent[value.Start].Equals(value.End))
+            {
+                // Back edge found
+                HasCycle = true;
+            }
+
+            if( !parent.ContainsKey(value.Start) )
+            {
+                parent.Add(value.Start, value.End);
+            }
+        }
+
+        public void PostVisit(IEdge<V, E> value)
+        {
+            
+        }
+    }
+}
