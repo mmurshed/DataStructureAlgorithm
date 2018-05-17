4c3600e1d12926a65fc47681903bea99937c9764
Mahbub Murshed <murshed@gmail.com>
2018-05-16 23:02:15 -0500

    Spiral matrix problem

---
+++ Algorithm.NUnit/SpiralMatrixTest.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithm.NUnit
+{
+	[TestFixture()]
+    public class SpiralMatrixTest
+    {
+		[TestCaseSource(nameof(TestCases))]
+        public void Test(int[,] matrix, int[] expected)
+        {
+            // Arrange
+			var itw = new Algorithm.MicrosoftProblems.SpiralMatrix();
+
+            // Act
+			var result = itw.SpiralOrder(matrix);
+
+            // Assert
+			CollectionAssert.AreEqual(expected, result);
+        }
+
+		static object[] TestCases =
+        {
+			new object[]
+            {
+                new int[,]
+                {
+                    { 0, 1, 2}
+                },
+                new int[] { 0, 1, 2}
+            },
+			new object[]
+            {
+                new int[,]
+                {
+                    { 1, 2, 3 },
+                    { 4, 5, 6 },
+                    { 7, 8, 9 }
+                },
+                new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 }
+            },
+			new object[]
+            {
+                new int[,]
+                {
+                    { 0, 1, 2, 3 },
+                    { 4, 5, 6, 7 },
+                    { 8, 9, 10, 11 }
+                },
+                new int[] { 0, 1, 2, 3, 7, 11, 10, 9, 8, 4, 5, 6 }
+            },
+			new object[]
+            {
+                new int[,]
+                {
+                    { 0, 1, 2, 3 },
+                    { 4, 5, 6, 7 },
+                    { 8, 9, 10, 11 },
+					{ 12, 13, 14, 15 }
+                },
+                new int[] { 0, 1, 2, 3, 7, 11, 15, 14, 13, 12, 8, 4, 5, 6, 10, 9 }
+            },        
+			new object[]
+            {
+                new int[,]
+                {
+                    { 0, 1, 2},
+                    { 4, 5, 6 },
+                    { 8, 9, 10 },
+                    { 12, 13, 14 }
+                },
+                new int[] { 0, 1, 2, 6, 10, 14, 13, 12, 8, 4, 5, 9 }
+            },
+			new object[]
+            {
+                new int[,]
+				{
+					{0},
+					{1},
+					{2}
+                },
+                new int[] { 0, 1, 2}
+            },        
+            new object[]
+            {
+                new int[,]
+                {
+                    {0}
+                },
+                new int[] {0}
+            },
+			new object[]
+            {
+                new int[,]
+                {
+                    {}
+                },
+                new int[] {}
+            }
+		};
+    }
+}
---
+++ Algorithm/MicrosoftProblems/SpiralMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm.MicrosoftProblems
+{
+	// https://leetcode.com/problems/spiral-matrix/description/
+    public class SpiralMatrix
+    {
+		public IList<int> SpiralOrder(int[,] matrix)
+        {
+			if (matrix == null || matrix.Length == 0)
+				return new List<int>();
+            int m = matrix.GetLength(0);
+			int n = matrix.GetLength(1);
+
+			var list = new List<int>(m * n);
+
+			int i = 0;
+			int j = 0;
+			while(i < m && j < n)
+			{
+				SpiralOrder(matrix, i, m, j, n, list);
+				i++; j++;
+				m--; n--;
+			}
+
+			return list;
+        }
+
+		private void SpiralOrder(int[,] matrix, int i, int m, int j, int n, IList<int> list)
+		{
+			// Top
+			for (int x = j; x < n; x++)
+				list.Add(matrix[i, x]);
+
+            // Right
+			for (int y = i + 1; y < m; y++)
+				list.Add(matrix[y, n - 1]);
+
+			// Bottom
+			if (i != m - 1)
+			{
+				for (int x = n - 2; x >= j && i < m; x--)
+					list.Add(matrix[m - 1, x]);
+			}
+
+			// Left
+			if (j != n - 1)
+			{
+				for (int y = m - 2; y > i && j < n; y--)
+					list.Add(matrix[y, j]);
+			}
+		}
+	}
+}
