145a3a5adfaf5714fda273b9565b537000ad8873
Mahbub Murshed <murshed@gmail.com>
2018-06-07 22:48:23 -0500

    Added naive solution for longest arithmatic progression

--- Algorithm/DynamicProgramming/Micellaneous/LongestArithmeticProgression.cs
+++ Algorithm/DynamicProgramming/Micellaneous/LongestArithmeticProgression.cs
@@ -22,9 +22,9 @@ A simple solution is to one by one consider every pair as first two elements of
 AP and check for the remaining elements in sorted set. To consider all pairs as 
 first two elements, we need to run a O(n^2) nested loop. Inside the nested 
 loops, we need a third loop which linearly looks for the more elements in 
-Arithmetic Progression (AP). This process takes O(n3) time.
+Arithmetic Progression (AP). This process takes O(n^3) time.
 
-We can solve this problem in O(n2) time using Dynamic Programming. To get idea 
+We can solve this problem in O(n^2) time using Dynamic Programming. To get idea 
 of the DP solution, let us first discuss solution of following simpler problem.
 
 Given a sorted set, find if there exist three elements in Arithmetic Progression 
@@ -33,7 +33,15 @@ or not
 Please note that, the answer is true if there are 3 or more elements in AP, 
 otherwise false.
 
-To find the three elements, we first fix an element as middle element and search for other two (one smaller and one greater). We start from the second element and fix every element as middle element. For an element set[j] to be middle of AP, there must exist elements ‘set[i]’ and ‘set[k]’ such that set[i] + set[k] = 2*set[j] where 0 <= i < j and j < k <=n-1. How to efficiently find i and k for a given j? We can find i and k in linear time using following simple algorithm.
+To find the three elements, we first fix an element as middle element and search
+for other two (one smaller and one greater). We start from the second element 
+and fix every element as middle element. For an element set[j] to be middle of 
+AP, there must exist elements ‘set[i]’ and ‘set[k]’ such that 
+set[i] + set[k] = 2*set[j] where 0 <= i < j and j < k <=n-1. 
+
+How to efficiently find i and k for a given j?
+We can find i and k in linear time using following simple algorithm.
+
 1) Initialize i as j-1 and k as j+1
 
 2) Do following while i >= 0 and j <= n-1 ..........a) If set[i] + set[k] is 
@@ -46,17 +54,22 @@ equal to 2*set[j], then we are done.
 // The function returns true if there exist three elements in AP
 // Assumption: set[0..n-1] is sorted.
 // The code strictly implements the algorithm provided in the reference. 
-bool arithmeticThree(int set[], int n)
+bool ArithmeticThree(int[] set, int n)
 {
   // One by fix every element as middle element
   for (int j=1; j<n-1; j++) {
-  // Initialize i and k for the current j int i = j-1, k = j+1;
-  // Find if there exist i and k that form AP 
-  // with j as middle element 
-  while (i >= 0 && k <= n-1) {
-      if (set[i] + set[k] == 2*set[j]) 
-          return true; 
-          (set[i] + set[k] < 2*set[j])? k++ : i--; 
+      // Initialize i and k for the current j
+      int i = j-1, k = j+1;
+
+      // Find if there exist i and k that form AP 
+      // with j as middle element 
+      while (i >= 0 && k <= n-1) {
+          if (set[i] + set[k] == 2*set[j])
+              return true; 
+          if(set[i] + set[k] < 2*set[j])
+            k++;
+          else
+            i--; 
       }
   }
   return false; 
@@ -66,22 +79,52 @@ How to extend the above solution for the original problem?
 The above function returns a boolean value. The required output of original 
 problem is Length of the Longest Arithmetic Progression (LLAP) which is an 
 integer value. If the given set has two or more elements, then the value of 
-LLAP is at least 2 (Why?).
+LLAP is at least 2, because 2 element will always create an
+arithmatic progression.
 
-The idea is to create a 2D table L[n][n]. An entry L[i][j] in this table stores 
+The idea is to create a 2D table L[n, n]. An entry L[i, j] in this table stores 
 LLAP with set[i] and set[j] as first two elements of AP and j > i. The last 
-column of the table is always 2 (Why – see the meaning of L[i][j]). Rest of 
+column of the table is always 2 (Why – see the meaning of L[i, j]). Rest of 
 the table is filled from bottom right to top left. To fill rest of the table, 
 j (second element in AP) is first fixed. i and k are searched for a fixed j. 
-If i and k are found such that i, j, k form an AP, then the value of L[i][j] 
-is set as L[j][k] + 1. Note that the value of L[j][k] must have been filled 
+If i and k are found such that i, j, k form an AP, then the value of L[i, j] 
+is set as L[j, k] + 1. Note that the value of L[j, k] must have been filled 
 before as the loop traverses from right to left columns.
     */
     public class LongestArithmeticProgression
     {
-        // Returns length of the longest AP subset in a given set
-        private int LenghtOfLongestAP(int[] set, int n)
+        // Assumption sorted
+        // O(n^3)
+        public int LengthOfLongestAPNaive(int[] set)
         {
+            int n = set.Length;
+            if (n <= 2)
+                return 2;
+            int max = 2;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int diff = set[i] - set[j];
+                    int len = 2;
+                    for (int k = j + 1; k < i; k++)
+                    {
+                        if (set[k] - set[j] == diff)
+                            len++;
+                    }
+                    max = Math.Max(max, len);
+                }
+            }
+
+            return max;
+        }
+
+        // Returns length of the longest AP subset in a given set,
+        // which is SORTED
+        public int LenghtOfLongestAP(int[] set)
+        {
+            int n = set.Length;
             if (n <= 2)
                 return n;
 
@@ -105,18 +148,18 @@ before as the loop traverses from right to left columns.
                 int k = j + 1;
                 while (i >= 0 && k <= n-1)
                 {
-                   if (set[i] + set[k] < 2*set[j])
-                       k++;
-         
-                   // Before changing i, set L[i,j] as 2
-                   else if (set[i] + set[k] > 2*set[j])
-                   {
+                    if (set[i] + set[k] < 2 * set[j])
+                    {
+                        k++;
+                    }
+                    // Before changing i, set L[i,j] as 2
+                    else if (set[i] + set[k] > 2 * set[j])
+                    {
                         L[i, j] = 2;
                         i--;
                     }
-         
-                   else
-                   {
+                    else
+                    {
                         // Found i and k for j, LLAP with i and j as first two
                         // elements is equal to LLAP with j and k as first two
                         // elements plus 1. L[j, k] must have been filled
@@ -130,7 +173,7 @@ before as the loop traverses from right to left columns.
                         // current j
                         i--;
                         k++;
-                   }
+                    }
                 }
          
                 // If the loop was stopped due to k becoming more than
